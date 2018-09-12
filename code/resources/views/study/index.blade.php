28addcc27cd3dbb69f43807fcee44ab6392aac68
heshanSB <heshan.sandaruwan55875@gmail.com>
2018-09-12 10:52:39 +0530

    count of members, courses and facilitators

--- code/resources/views/study/index.blade.php
+++ code/resources/views/study/index.blade.php
@@ -174,12 +174,12 @@
 			<div class="inner_w3l_agile_grids row">
 				<div class="col-md-3 w3layouts_stats_left w3_counter_grid">
 				   <i class="fa fa-laptop" aria-hidden="true"></i>
-					<p class="counter">45</p>
+					<p class="counter">{{$courseCount}}</p>
 					<h3>Courses</h3>
 				</div>
 				<div class="col-md-3 w3layouts_stats_left w3_counter_grid1">
 				    <i class="fa fa-smile-o" aria-hidden="true"></i>
-					<p class="counter">165</p>
+					<p class="counter">{{$users}}</p>
 					<h3>Members</h3>
 				</div>
 				<!--<div class="col-md-3 w3layouts_stats_left w3_counter_grid2">
@@ -189,7 +189,7 @@
 				</div>-->
 				<div class="col-md-3 w3layouts_stats_left w3_counter_grid3">
 				<i class="fa fa-user" aria-hidden="true"></i>
-					<p class="counter">245</p>
+					<p class="counter">{{$facilitatorCount}}</p>
 					<h3>Facilitators</h3>
 				</div>
 			</div>
