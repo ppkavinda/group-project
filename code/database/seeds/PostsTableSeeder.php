1a2120fe9282f3aa6511a8cf7b1b785e2448fb5b
sachintha29 <sachin133hm@gmail.com>
2018-09-15 11:22:22 +0530

    images

--- code/database/seeds/PostsTableSeeder.php
+++ code/database/seeds/PostsTableSeeder.php
@@ -23,5 +23,29 @@ class PostsTableSeeder extends Seeder
             'body' => 'Sed quis eleifend leo. Phasellus iaculis, metus facilisis gravida dapibus, ligula dolor placerat dolor, eget cursus neque risus quis tortor varius augue ut mauris condimentum dictum.',
             'user_id' => 1,
         ]);
+        
+        \App\Post::create([
+            'title' => 'Chilli Sauce Manufacturing ',
+            'course_id' => 3,
+            'body' => '
+            <h3>Ingredients</h3>:
+            Chilli 100<br>
+            400 tomatoes<br>
+            Pepper 2 tablespoons tables<br>
+            100 onions<br>
+            White toddy<br>
+            A piece of ginger<br>
+            Stock Water bottles 01<br>
+            Vinegar 1/2 bottles<br>
+            Sugar little<br>
+            4 tablespoons of margarine tables<br>
+            Plum teaspoons 02',
+            'user_id' => 1,
+        ]);
+        
+
+         
+
+
     }
 }
