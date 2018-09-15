c5afbe4c818650bdbc4ca8a1b0f6725f08649b09
prasad kavinda <pp.kavinda@gmail.com>
2018-09-15 10:45:02 +0530

    fixed login/register

--- code/resources/views/auth/login.blade.php
+++ code/resources/views/auth/login.blade.php
@@ -1,32 +1,29 @@
-@extends('admin.master')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <!-- <div class="col-md-8"> -->
-        <div class="register-box">
-            <div class="row justify-content-center">
+        <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
-                    <p class="login-box-msg" style="color:DodgerBlue;font-size:110%;"><b>Login to your Athwela Account!</b></p>
+                    <p style="color:DodgerBlue;font-size:110%;" class="text-center"><b>{{ __('Sign Up and Start Learning!') }}</b></p>
                 </div>
 
                 <div class="login-logo">
-                    <a href="/"><b><img src="/dist/img/logo.jpg" alt="logo" style="width: 30%; opacity: .4"></a>
+                    <a href="/"><img src="/dist/img/logo.jpg" alt="logo" style="width: 30%; opacity: .4"></a>
                 </div>
                 <div class="card-body">
-
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" align="left" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-8">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="email" name="email" value="{{ old('email') }}" required autofocus>
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
@@ -36,11 +33,11 @@
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-8">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="password" name="password" required>
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
@@ -49,13 +46,16 @@
 
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
                                     </label>
                                 </div>
                             </div>
                         </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
@@ -67,22 +67,10 @@
                                 </a>
                             </div>
                         </div>
-
-                        <div class="social-auth-links text-center mb-3">
-                             <p>- OR -</p>
-                            <a href="#" class="btn btn-block btn-primary">
-                                <i class="fa fa-facebook mr-2"></i> Sign in using Facebook
-                            </a>
-                            <a href="#" class="btn btn-block btn-danger">
-                                <i class="fa fa-google-plus mr-2"></i> Sign in using Google+
-                            </a>
-                        </div>
-                    <!-- /.social-auth-links -->
                     </form>
                 </div>
             </div>
         </div>
-         </div>
     </div>
 </div>
 @endsection
