ffc5d135b512fdcb30e193b5aebcd4fc0299a829
sachintha29 <sachin133hm@gmail.com>
2018-09-16 17:14:14 +0530

    Post.php,PostableSeeder.php,Postcard.blade.php

--- code/resources/views/study/courses/postCard.blade.php
+++ code/resources/views/study/courses/postCard.blade.php
@@ -4,6 +4,7 @@
         <div class="card " style="border: 1px solid gray">
         <div class="card-body">
             <h4 class="card-title">{!! $post->title !!} </h4>
+            
             <p><small class="text-success">{{ $post->created_at->diffForHumans() }}</small></p>
 
             <p class="card-text">{{ $post->snippet() }}</p>
