8e0bf6262a8b5384c217d2e8c63536e55e4ee736
prasad kavinda <pp.kavinda@gmail.com>
2018-09-26 02:42:46 +0530

    added some middlewares

---
+++ code/app/Http/Middleware/Facilitator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Facilitator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->user()->role > 2) {
+            abort(404);
+        }
+        return $next($request);
+    }
+}
