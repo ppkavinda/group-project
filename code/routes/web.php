02fed5d2a92fc8da579c162bf4eba437bc72a303
prasad kavinda <pp.kavinda@gmail.com>
2018-10-03 09:04:12 +0530

    added shop interfaces

--- code/routes/web.php
+++ code/routes/web.php
@@ -26,6 +26,9 @@ Route::post('/users/{user}/edit', 'UserController@edit');
 Route::get('/user', 'UserController@get');
 // shop
 Route::view('/shop', 'shop.index');
+Route::view('/mens', 'shop.mens');
+Route::view('/woman', 'shop.woman');
+Route::view('/single', 'shop.single');
 
 // study
 Route::view('/study', 'study.index');
