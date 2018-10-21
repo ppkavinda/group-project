defd4884c5b84958e1509bccfb337c5b92b8cce3
sachintha29 <sachin133hm@gmail.com>
2018-10-21 23:02:01 +0530

    change some

--- code/resources/views/admin/partial/sidebar.blade.php
+++ code/resources/views/admin/partial/sidebar.blade.php
@@ -23,28 +23,13 @@
                 <i class="right fa fa-angle-left"></i>
               </p>
             </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="./index.html" class="nav-link active">
-                  <i class="fa fa-circle-o nav-icon"></i>
-                  <p>Dashboard v1</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index2.html" class="nav-link">
-                  <i class="fa fa-circle-o nav-icon"></i>
-                  <p>Dashboard v2</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index3.html" class="nav-link">
-                  <i class="fa fa-circle-o nav-icon"></i>
-                  <p>Dashboard v3</p>
-                </a>
-              </li>
-            </ul>
-          </li>
           
+          </li>
+          <a href="index3.html" class="brand-link">
+    <img src="/dist/img/logo.jpg"  alt="logo"  class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light"> Learining Centre</span>
+    </a>
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
               <i class="nav-icon fa fa-edit"></i>
@@ -72,7 +57,11 @@
               </ul>
 
 
-
+ <a href="index3.html" class="brand-link">
+    <img src="/dist/img/logo.jpg"  alt="logo"  class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light"> Shoping Centre</span>
+    </a>
 
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
