2c9da4fa2b135151253b1f55563f3bc4eba56f11
prasad kavinda <pp.kavinda@gmail.com>
2018-12-02 00:32:35 +0530

    add some logs

--- code/server.js
+++ code/server.js
@@ -19,6 +19,10 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
+app.post('*', (req, res) => {
+    console.log('hit')
+})
+
 app.post('/checkout/notify', (req, res) => {
     let sql1 = `INSERT INTO payments 
                     (id, amount, currency, status_code, order_id, created_at, updated_at)
@@ -28,10 +32,16 @@ app.post('/checkout/notify', (req, res) => {
 
     let data = [req.body.payment_id, req.body.payhere_amount, req.body.payhere_currency, req.body.status_code, req.body.order_id, new Date(), new Date()]
 
-    connection.query(sql1, data)
+    connection.query(sql1, data, function (err, result) {
+        if (!err) {
+            console.log('inserted', result)
+        }
+    })
     connection.query(sql2, [req.body.payment_id, req.body.order_id])
 
     res.sendStatus(200)
 })
 
-app.listen(port)
+app.listen(port, function () {
+    console.log('server started')
+})
