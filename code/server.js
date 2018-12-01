a66775a78eedad19a04afb0b7eb2b77cd5776e07
prasad kavinda <pp.kavinda@gmail.com>
2018-12-02 00:59:53 +0530

    remove unnecessory code

--- code/server.js
+++ code/server.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser')
 const mysql = require('mysql')
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'prasad',
-    password: 'hondaz',
+    user: 'athwelauser',
+    password: 'Tw200bike',
     database: 'athwela'
 })
 const app = express()
@@ -19,10 +19,6 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-app.post('*', (req, res) => {
-    console.log('hit')
-})
-
 app.post('/checkout/notify', (req, res) => {
     let sql1 = `INSERT INTO payments 
                     (id, amount, currency, status_code, order_id, created_at, updated_at)
@@ -33,13 +29,13 @@ app.post('/checkout/notify', (req, res) => {
     let data = [req.body.payment_id, req.body.payhere_amount, req.body.payhere_currency, req.body.status_code, req.body.order_id, new Date(), new Date()]
 
     connection.query(sql1, data, function (err, result) {
-        if (!err) {
-            console.log('inserted', result)
+        if (err) {
+            console.log('err1', err)
         }
     })
     connection.query(sql2, [req.body.payment_id, req.body.order_id], function (err, result) {
-        if (!err) {
-            console.log('inserted', result)
+        if (err) {
+            console.log('err1', err)
         }
     })
 
